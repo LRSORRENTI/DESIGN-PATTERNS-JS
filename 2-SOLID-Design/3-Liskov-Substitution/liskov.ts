058560c42270f3629e6f7588ebe35985306fd1ea
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-01 08:35:26 -0500

    Added more to liskov notes, implementation guidelines

---
+++ 2-SOLID-Design/3-Liskov-Substitution/liskov.ts
@@ -0,0 +1,30 @@
+/* 
+============================
+LISKOV SUBSTITUTION AKA: LSP
+============================
+
+Now let's go over the Liskov principle, 
+introduced by Barbara Liskov, during a 
+comference on data abstraction and 
+hierarchy
+
+let's see the implementation guidelines of LSP
+
+Substitution is a principle of OOP, 
+and it states that: 
+
+"S" is a subtype of "T", then objects 
+of type "T" may be replaced with objects 
+of type "S"
+
+Which means derived types must be completely 
+subsitutable for their base types 
+
+LSP is a particular definition of a subtyping 
+relation called STRONG BEHAVIORAL SUBTYPING 
+
+It can be seen as an extension of the open-closed 
+principle
+
+*/
+
