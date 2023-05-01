dc377bd1f3eb87eae23b32ee2e8be7012ff79597
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-01 14:26:52 -0500

    added more to LSP of SOLID

---
+++ 2-SOLID-Design/3-Liskov-Substitution/LSP.js
@@ -0,0 +1,37 @@
+/* 
+=================
+Substitutability
+=================
+
+In OOP, it's said that: 
+
+If S is a subtype of T,
+then objects of type T,
+are interchangeable with
+objects of type S
+
+This means that if a program is written
+ to work with objects of type T, 
+ it should be able to work just as 
+ well with objects of type S, 
+ as long as S is a subtype of T. 
+ 
+ In other words, objects of type S 
+ should be able to seamlessly substitute
+  for objects of type T in any part of 
+  the program without causing any errors
+   or unexpected behavior.
+
+By following the Liskov Substitution 
+Principle, we can create more flexible
+ and extensible code that is easier 
+ to maintain and modify over time. 
+ 
+ It allows us to write code that can
+  be easily extended or modified by 
+  adding new subtypes, without having 
+  to rewrite large portions of the 
+  existing code.
+
+
+*/
