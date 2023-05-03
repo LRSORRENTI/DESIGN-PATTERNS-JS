3ceb71a10f2e24a3a1646a3c01c3198f440c2acc
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-03 08:18:52 -0500

    finished section on Interface Segregation principle, avoiding bloated interfaces whenever possible

--- 02-SOLID-Design/4-Interface-Segregation/interSeg.ts
+++ 02-SOLID-Design/4-Interface-Segregation/interSeg.ts
@@ -22,6 +22,12 @@ of SRP, one interface shouldn't have excess
 responsibility, instead multiple intefaces should 
 handle different responsibilities
 
+Essentially, instead of implementing one giant 
+interface, segregate functionality into smaller
+interfaces, sort of like an extension of SRP, one 
+interface shouldn't be handling an excess of 
+responsibilities 
+
 */
 
 // Let's look at a real world example, 
@@ -31,7 +37,7 @@ handle different responsibilities
 
 // But it's possible there's a single 
 // type of ATM that has both 
-// kinds of functionality
+// kinds of functionality, withdrawal and deposit
 
 // The advantage of having separate ATM's 
 // is that the people who need to deposit or 
@@ -45,29 +51,121 @@ handle different responsibilities
 // most of the functionality of a given interface,
 // this InterSeg principle has been violated 
 
+// interface Store {
+//     // We have an interface called Store 
+//     // This interface exposes four funcs
+//     // let's say we have a database class 
+//     // that implements this interface
+//     save();
+//     delete();
+//     update();
+//     fetch();
+// }
+
+// class DBStore implements Store {
+//     save(){
+
+//     }
+
+//     delete() {
+        
+//     }
+//     update() {
+        
+//     }
+//     fetch() {
+        
+//     }
+// }
+
+// class CacheStore implements Store {
+//     save() {
+        
+//     }
+//     delete() {
+        
+//     }
+//     update() {
+        
+//     }
+//     fetch() {
+        
+//     }
+//     // Implemented functions:
+//     AddKey(){
+
+//     }
+//     RemoveKey(){
+
+//     }
+//     EditKey(){
+
+//     }
+// }
+
+
+// Now let's say we add these implemented 
+// functions to Store: 
+
 interface Store {
-    // We have an interface called Store 
-    // This interface exposes four funcs
-    // let's say we have a database class 
-    // that implements this interface
-    save();
-    delete();
-    update();
-    fetch();
-}
+        save();
+        delete();
+        update();
+        fetch();
+        AddKey();
+        RemoveKey();
+        EditKey();
+        // from the perspective of Store
+        // it makes sense to have these functions 
 
-class DBStore implements Store {
-    save(){
+        // And now the CacheStore can also access the 
+        // main four functions from Store, as well 
+        // as the Add, Remove, and EditKey funcs 
+
+        // But the main four funcs, these four also 
+        // need to be written in the CacheStore class
+        // which is implementing Store
+
+        // But this makes our Store interface 'fat', and 
+        // if we needed to add more functionality the 
+        // Store interface would get more and more bloated
 
     }
 
-    delete() {
+    // This is a violation of the Interface 
+        // Segregation principle, the better 
+        // solution is to create two interfaces
+
+        // One interface to implement the main 
+        // four funcs :
+
+        interface DBStoreService {
+            Save();
+            Delete();
+            Update();
+            Fetch();
+        }
+       
+        interface CacheStoreService{
+            Addkey();
+            DeleteKey();
+            EditKey();
+        }
+
+        // In essence, we want to keep functionality 
+        // simple across interfaces, having gigantic 
+        // interfaces is not 
+class NextStore implements CacheStoreService{
+    Addkey() {
         
     }
-    update() {
+    DeleteKey() {
         
     }
-    fetch() {
+    EditKey() {
+        
+    }
+    NextStore(){
         
     }
 }
