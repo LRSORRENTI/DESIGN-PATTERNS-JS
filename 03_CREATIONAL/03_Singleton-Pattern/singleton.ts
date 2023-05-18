fd208511796bb213107e1cfda3e6aaccf41cdfa7
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-18 07:46:41 -0500

    added intro to singleton design pattern, added diagram displaying general concept

---
+++ 03_CREATIONAL/03_Singleton-Pattern/singleton.ts
@@ -0,0 +1,27 @@
+/*
+
+==================
+SINGLETON PATTERN
+==================
+
+What is the singleton pattern? 
+
+It's when we create a single object, that shares 
+resources throughout your application, 
+
+It looks like: 
+
+
+         OBJECT A-------------   --------OBJECT B
+                            |   |
+                          SINGLETON
+                        SHARED RESOURCE
+                         STORED STATE
+                            |  |
+        OBJECT C-------------  -------------OBJECT D
+
+
+
+
+
+*/
