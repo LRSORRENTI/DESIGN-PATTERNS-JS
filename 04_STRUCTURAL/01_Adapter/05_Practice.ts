f2567a3362e80f3f4a90343a7a98a2f85f8cee08
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-22 09:45:50 -0500

    okay logic is working I think, I tried to add comments to explain what steps are taking place as the new objects are instantiated

--- 04_STRUCTURAL/01_Adapter/05_Practice.ts
+++ 04_STRUCTURAL/01_Adapter/05_Practice.ts
@@ -13,18 +13,18 @@
 //   it in such a way that it can be used as
 //    an argument to calculateArea().
 
-class Square
-{
-  constructor(side)
-  {
-    this.side = side;
-  }
-}
+// class Square
+// {
+//   constructor(side)
+//   {
+//     this.side = side;
+//   }
+// }
 
-function area(rectangle)
-{
-  return rectangle._width * rectangle._height;
-}
+// function area(rectangle)
+// {
+//   return rectangle._width * rectangle._height;
+// }
 
 
 // build an adapter called SquareToRectangleAdapter
@@ -57,18 +57,102 @@ function area(rectangle)
 // be used in area(), the adapter will need 
 // width and height properties 
 
-class SquareToRectangleAdapter extends Square{
-    // we need to adapt Square in here, to 
-    // adapt a Sqaure object to behave like 
-    constructor(square) {
-        super(square.side); 
-        // Call the parent class
-        // constructor with the side 
-        //length of the square
+// class SquareToRectangleAdapter extends Square{
+//     // we need to adapt Square in here, to 
+//     // adapt a Sqaure object to behave like 
+//     constructor(square) {
+//         super(square.side); 
+//         // Call the parent class
+//         // constructor with the side 
+//         //length of the square
     
-        // Add the necessary properties to simulate a rectangle
-        this._width = square.side;
-        this._height = square.side;
-      }
-}
+//         // Add the necessary properties to simulate a rectangle
+//         this._width = square.side;
+//         this._height = square.side;
+//       }
+// }
 
+// class Square {
+//          constructor(side) {
+//             this.side = side;
+//           }
+//     }
+  
+  
+//     class SquareToRectangleAdapter extends Square {
+//         constructor(square) {
+//           super(square.side);
+//           this._width = square.side;
+//           this._height = square.side * 2; // Assuming a rectangle with double the height of the square
+//         }
+//       }
+      
+// // So now I need to actually create a new instance 
+// // object of it 
+
+// const sqRect = new SquareToRectangleAdapter(123);
+
+// console.log(sqRect)
+
+// function area(rectangle)
+// {
+//   return rectangle._width * rectangle._height;
+// }
+
+// area(sqRect)
+
+class Square {
+    side: number;
+
+    // This class Square represents
+    //  a square shape.
+
+    // It has a single property side of type number.
+    // The constructor takes a side
+    //  parameter and assigns it to the
+    //   side property of the class instance.
+    constructor(side: number) {
+      this.side = side;
+    }
+  }
+  
+    // This class SquareToRectangleAdapter adapts
+    //  a Square object to behave like a rectangle.
+
+  class SquareToRectangleAdapter {
+    private square: Square;
+    // It has a private property square of type
+    //  Square to store the adapted square.
+  
+    constructor(square: Square) {
+      this.square = square;
+    //   The constructor takes a Square object 
+    //   as a parameter and assigns 
+    //   it to the square property.
+    }
+  
+    get _width() {
+      return this.square.side;
+    //   The get _width() method returns
+    //    the width of the adapted rectangle,
+    //    which is equal to the side 
+    //    length of the square.
+    }
+  
+    get _height() {
+      return this.square.side * 2;
+    //   The get _height() method returns
+    //    the height of the adapted rectangle,
+    //    which is twice the side length of the square.
+    }
+  }
+  
+  const square = new Square(123);
+  // we then create new instances of square 
+  // and sqToRect with the 'new' keyword
+const sqRect = new SquareToRectangleAdapter(square);
+
+// now we can also access the values of the 
+// adapted width and height properties
+
+console.log(sqRect._width, sqRect._height);  // Output: 123 246
