1d02abf3b1319750a91d37a194c889cc89b3fcee
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-24 09:35:30 -0500

    added setup for typescript bridge pattern example

---
+++ 04_STRUCTURAL/02_Bridge/03_Bridge.ts
@@ -0,0 +1,20 @@
+// TYPESCRIPT EXAMPLE
+
+// To illustrate the bridge pattern, we'll 
+// look at an example of a payment processor, 
+// with two different companies: 
+
+class OnlinePaymentProcessor {
+      processPayment(){
+
+      }
+}
+
+
+class ElementSkateboards {
+    name: string = 'Element Skateboards'
+}
+
+class BirdhouseSkateboards {
+   name: string = 'Birdhouse Skateboards'
+}
