ffe93edb71bf278da4bfa8b1f2a3ce852a45d639
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-24 09:58:31 -0500

    added typescript example of bridge pattern, using interface to define a process payment, we can now use any kind of payment processor we define, like make a stripe class and use that instead

--- 04_STRUCTURAL/02_Bridge/03_Bridge.ts
+++ 04_STRUCTURAL/02_Bridge/03_Bridge.ts
@@ -4,17 +4,48 @@
 // look at an example of a payment processor, 
 // with two different companies: 
 
-class OnlinePaymentProcessor {
-      processPayment(){
+interface OPaymentProcessor{
+    processPayment(payment: string): void
+}
 
+class OnlinePaymentProcessor {
+      processPayment(payment: string){
+          console.log("Payment being processed...");
+          console.log(payment);
+          console.log("Payment finalized");
       }
 }
 
 
-class ElementSkateboards {
-    name: string = 'Element Skateboards'
+class SkateboardCompany1 {
+    name: string = 'Company 1'
+   private paymentProcessor: OPaymentProcessor;
+    constructor(paymentProcessor: OPaymentProcessor){
+         this.paymentProcessor = paymentProcessor
+    }
+
+    processPayment(){
+        console.log('Company 1 processing payment')
+       this.paymentProcessor.processPayment('first payment')
+    }
 }
 
-class BirdhouseSkateboards {
-   name: string = 'Birdhouse Skateboards'
+class SkateboardCompany2 {
+    name: string = 'Company 1'
+    private paymentProcessor: OPaymentProcessor;
+     constructor(paymentProcessor: OPaymentProcessor){
+          this.paymentProcessor = paymentProcessor
+     }
+ 
+     processPayment(){
+         console.log('Company 2 processing payment')
+        this.paymentProcessor.processPayment('first payment')
+     }
 }
+
+const BirdhouseSkateboards = new SkateboardCompany1(new OnlinePaymentProcessor())
+// We're injecting this payment processor in, 
+const ElementSkateboards = new SkateboardCompany2(new OnlinePaymentProcessor() )
+// So now we need to create an instance of company
+BirdhouseSkateboards.processPayment();
+ElementSkateboards.processPayment();
